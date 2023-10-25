8b13df77a81b31b2597f044e4d3e96a5f2af7d0e
Martin Raum <martin@raum-brothers.eu>
2023-10-25 20:49:35 +0200

    add visual selection retrieval

--- lua/facilellm/util.lua
+++ lua/facilellm/util.lua
@@ -23,7 +23,30 @@ local win_vsplit_modifier = function (relative, direction)
   end
 end
 
+---@ return string[] | nil
+local get_visual_selection = function ()
+  local mode = vim.fn.mode()
+  local esckey = vim.api.nvim_replace_termcodes('<Esc>', true, true, true)
+  vim.api.nvim_feedkeys(esckey, 'nx', false)
+  local _, rs, cs = unpack(vim.fn.getpos("'<"))
+  local _, re, ce = unpack(vim.fn.getpos("'>"))
+
+  if mode == "v" then
+    return vim.api.nvim_buf_get_text(0, rs-1, cs-1, re-1, ce, {})
+  elseif mode == "V" then
+    return vim.api.nvim_buf_get_lines(0, rs-1, re, false)
+  elseif mode == "" then
+    local lines_buf = vim.api.nvim_buf_get_lines(0, rs-1, re, false)
+    local lines = {}
+    for _,l in ipairs(lines_buf) do
+      table.insert(lines, string.sub(l, cs, ce))
+    end
+    return lines
+  end
+end
+
 
 return {
   win_vsplit_modifier  = win_vsplit_modifier,
+  get_visual_selection = get_visual_selection,
 }

da87f4fe5a0ad2424690bfde0614df1cf9c33108
Martin Raum <martin@raum-brothers.eu>
2023-11-22 20:09:16 -0300

    refact: format exported functions

--- lua/facilellm/init.lua
+++ lua/facilellm/init.lua
@@ -2,28 +2,24 @@ local config = require("facilellm.config")
 local convenience = require("facilellm.convenience")
 
 
-local M = {}
+return {
+  setup = config.setup,
 
+  select_default_model                     = convenience.select_default_model,
+  create_from_model                        = convenience.create_from_model,
+  create_from_model_selection              = convenience.create_from_model_selection,
+  create_from_conversation_selection       = convenience.create_from_conversation_selection,
+  create_from_model_conversation_selection = convenience.create_from_model_conversation_selection,
+  delete_from_selection                    = convenience.delete_from_selection,
+  rename_from_selection                    = convenience.rename_from_selection,
+  set_model_from_selection                 = convenience.set_model_from_selection,
 
-M.setup = config.setup
+  show                 = convenience.show,
+  focus                = convenience.focus,
+  focus_from_selection = convenience.focus_from_selection,
 
-M.select_default_model = convenience.select_default_model
-M.create_from_model = convenience.create_from_model
-M.create_from_model_selection = convenience.create_from_model_selection
-M.create_from_conversation_selection = convenience.create_from_conversation_selection
-M.create_from_model_conversation_selection = convenience.create_from_model_conversation_selection
-M.delete_from_selection = convenience.delete_from_selection
-M.rename_from_selection = convenience.rename_from_selection
-M.set_model_from_selection = convenience.set_model_from_selection
-
-M.show = convenience.show
-M.focus = convenience.focus
-M.focus_from_selection = convenience.focus_from_selection
-
-M.add_visual_as_input_and_query = convenience.add_visual_as_input_and_query
-M.add_visual_as_context = convenience.add_visual_as_context
-M.add_visual_as_instruction = convenience.add_visual_as_instruction
-M.add_visual_as_input_query_and_insert = convenience.add_visual_as_input_query_and_insert
-
-
-return M
+  add_visual_as_input_and_query        = convenience.add_visual_as_input_and_query,
+  add_visual_as_context                = convenience.add_visual_as_context,
+  add_visual_as_instruction            = convenience.add_visual_as_instruction,
+  add_visual_as_input_query_and_insert = convenience.add_visual_as_input_query_and_insert,
+}
